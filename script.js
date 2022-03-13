c1df7d2be58772a7bc54cea347dece6fbf36f46a
Aceer121 <45231128+Aceer121@users.noreply.github.com>
2022-03-12 21:51:09 -0500

    Array missing

---
+++ script.js
@@ -0,0 +1,17 @@
+let apiQuotes = [];
+
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiURL = 'https://type.fit/api/quotes';
+try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+} catch (error)
+    // Catch Error Here
+}
+
+
+//On Load
+getQuotes();
